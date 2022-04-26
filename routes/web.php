d2ecf2723c5e999aefa0893d66ee4ca0c7cb8ffd
adimerpaul <adimer101@gmail.com>
2022-04-26 14:02:02 -0400

    modificado a ala antigua ruta

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,16 @@ Route::get('/cliente', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::apiResource('/unit',\App\Http\Controllers\UnitController::class);
-Route::apiResource('/ticket',\App\Http\Controllers\TicketController::class);
-Route::post('/login',[\App\Http\Controllers\EmpleadoController::class,'login']);
-Route::post('/atender',[\App\Http\Controllers\EmpleadoController::class,'atender']);
-Route::post('/ultificha',[\App\Http\Controllers\EmpleadoController::class,'ultificha']);
-Route::post('/datosatender',[\App\Http\Controllers\EmpleadoController::class,'datosatender']);
+Route::apiResource('/unit','UnitController');
+Route::apiResource('/ticket','ticketController');
+Route::post('/login','EmpleadoController@login');
+Route::post('/atender','EmpleadoController@atender');
+Route::post('/ultificha','EmpleadoController@ultificha');
+Route::post('/datosatender','EmpleadoController@datosatender');
+
+//Route::apiResource('/unit',\App\Http\Controllers\UnitController::class);
+//Route::apiResource('/ticket',\App\Http\Controllers\TicketController::class);
+//Route::post('/login',[\App\Http\Controllers\EmpleadoController::class,'login']);
+//Route::post('/atender',[\App\Http\Controllers\EmpleadoController::class,'atender']);
+//Route::post('/ultificha',[\App\Http\Controllers\EmpleadoController::class,'ultificha']);
+//Route::post('/datosatender',[\App\Http\Controllers\EmpleadoController::class,'datosatender']);
